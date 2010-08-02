701c833153fa8fccada8638a03966d12991ad955
tnyholm <tnyholm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-02 14:13:09 +0000

    adding first version of seo-ftoc
    git-svn-id: https://plugins.svn.wordpress.org/seo-friendly-table-of-contents/trunk@270646 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Seo Friendly Table of Contents ===
+Contributors: tnyholm
+Tags: toc, Post, page, content, table of contents, seo, links
+Requires at least: 2.8.1
+Tested up to: 3.0.1
+Stable tag: trunk
+
+A simple seo friendly table of contents plugin that does not requere editing in your themes source code.
+== Description ==
+This plugin puts a table of contents on your page. You just need to type `[toc="2,3" title="Table of contents"]` in the page/post editor. That's it!
+You dont need to edit your themes source code. The plugin allows you to set a title of the toc box. You can also choose which heading tags (h1,h2,h3,h4 etc)
+you want to have in the table of contents. For more information see installation page or the 
+[Webfish](http://www.webfish.se/wp/plugins/seo-friendly-table-of-contents "Webfish - Wordpress plugin") homepage.
+
+== Installation ==
+
+This is some brief installation notes. For more information see [Webfish](http://www.webfish.se/wp/plugins "Wordpress plugins")
+
+1. Upload the plugin source to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Put `[toc="2,3" title="Table of contents"]` on a page with at least one h2 heading.
+
+== Frequently Asked Questions ==
+
+The frequently asked questions is updated at the plugin's homepage [Growyn Search plugin](http://www.webfish.se/wp/plugins/seo-friendly-table-of-contents#FAQ "Webfish - Wordpress plugin"). 
+
---
+++ seo-ftoc.php
@@ -0,0 +1,161 @@
+<?php
+/*
+ Plugin Name: Seo Friendly Table of Contents
+ Plugin URI: http://www.webfish.se/wp/plugins/seo-friendly-table-of-contents
+ Description: Adds a seo firendly table of contents anywhere you write [toc="2,3,4" title="Table of contents"].
+ Version: 1.3
+ Author: Tobias Nyholm
+ Author URI: http://www.tnyholm.se
+
+/*
+    This file is part of Seo Friendly Table of Contents.
+
+    Seo Friendly Table of Contents is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Seo Friendly Table of Contents is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Seo Friendly Table of Contents.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
+add_filter('the_content', 'seotocFilter');
+
+//add css
+wp_enqueue_style('toc_css',WP_PLUGIN_URL.'/seo-ftoc/style.css');
+
+
+/**
+ * Verifies if a table of contents should be here. If so, then we add one.
+ * @param String $content
+ */
+function seotocFilter($content){
+
+	//reges to match if the "[toc=2]" exists
+	$tolc_regex = '|(?:<p>)?\[toc=(?:["'."'".'])?([1-7,]+)(?:["'."'".'])?(?: title=(?:["'."'".'])?(.*?)(?:["'."'".'])?)? ?\](?:<br \/>)?(?:<\/p>)?|s';
+	$tolc_match = preg_match_all($tolc_regex, $content, $tolc_matches);
+	//echo ("<pre>".print_r($tolc_matches,true));
+
+	//if [toc] exists
+	if ($tolc_match){
+		//split toc parameters into a array
+		$tags=split(",",$tolc_matches[1][0]);
+
+		if($tags[count($tags)-1]=="")//if the user has a tailoring coma
+		unset($tags[count($tags)-1]);
+		//sort
+		sort($tags);
+
+		//assert: $tags[0] is the first heading to appear.
+
+		//get the title
+		$toc_title=$tolc_matches[2][0];
+		$list="";//allcolate some space
+
+		//ad ids to the headings
+		seotoc_addIds($tags,$content,$list);
+
+	
+		//wrapp the list into some divs and add the title if the list was not empty
+		if($list!="")
+			$list="<div id='toc'><div id='toc_title'>$toc_title</div>$list</div>";
+
+		//replace the [toc] with the $html
+		$content = preg_replace($tolc_regex, $list, $content,1);
+	}
+
+	return $content;
+}
+
+/**
+ * Add the some ids on h#-tags. It also build a html list with links to those tags.
+ * @param array $tagsArray, sorted
+ * @param String $content, 
+ * @param String $list, when the function has been called, this is a html list with heading links (if any)
+ * @return nothing $content and $list has the result
+ */
+function seotoc_addIds(array $tagsArray,&$content,&$list){
+	$tags="";
+	//make a string form the array
+	foreach($tagsArray as $tag)
+		if($tag<8)
+			$tags.="$tag";
+		
+	//some regex to recognize the h#-tags
+	$tag_regex = '/<h(['.$tags.'])(.*?)>(.*?)(<\/h['.$tags.']>)/';
+
+	$tag_match = preg_match_all($tag_regex, $content, $tag_matches);
+	//die ("<pre>".print_r($tag_matches,true));
+	
+	$lastLevel=-1;//init leven
+	$list="";//override $list
+	
+	//if a h#-tag found.
+	if ($tag_match){
+		foreach ($tag_matches[3] as $key => $title){
+			/*
+			 * Prepare some variables
+			 */
+			//original string
+			$orgStr=$tag_matches[0][$key];
+				
+			//strip any unwanted html
+			$noTagsTitle=strip_tags($title);
+			
+			//kind of ugly slugify. But it works well with special chars
+			$replace=array("/\&#[0-9]*?;/"=>"","/[^a-z A-Z0-9]/"=>"","/ /"=>"-");
+			$noSpecial= preg_replace(array_keys($replace),array_values($replace), $noTagsTitle);
+			
+			//this is the h#-tag
+			$tag="h".$tag_matches[1][$key];
+			
+			//save any attributes that the h#-tag allredy has. and then add the id.
+			$attributes=$tag_matches[2][$key]." id='$noSpecial'";
+			
+			//Set a level. 
+			$thisLevel=$tag_matches[1][$key];
+			
+
+			/*
+			 * prepare list
+			*/
+			if($thisLevel>$lastLevel){//start a sub list
+				$list.="\n<ul>";
+				$lastLevel=$thisLevel;
+			}
+			elseif($thisLevel<$lastLevel){//end a sublist
+				for($i=$lastLevel;$i>$thisLevel;$i--)
+					$list.="</ul></li>";
+				$lastLevel=$thisLevel;
+			}
+			else{//end a list item
+				$list.='</li>'."\n";
+			}
+			
+			//add a list item
+			$list.='<li><a href="#'.$noSpecial.'">'.$noTagsTitle.'</a>';//no closing li
+				
+			/*
+			 * write id
+			 */
+			//replace the original heading with the one with an ID
+			$content = str_replace($orgStr, "<$tag$attributes>$title</$tag>", $content);
+				
+		}
+		$list.='</li>'."\n";//close the last opened li
+		for($i=$lastLevel;$i>$tagsArray[0];$i--)//close all open tags
+			$list.="</ul></li>";
+		$list.= "</ul>\n";//close the last ul
+	}
+	//No need to return anyting. The $content and $list parameter is passed by reference.
+}
+
+
+?>
