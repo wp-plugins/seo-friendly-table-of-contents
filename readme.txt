42ded96dd6703d1aa23dbf3b8952fb7a48d82882
tnyholm <tnyholm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-26 10:15:41 +0000

    updated to 2.0.0. This version breaks backwards compatibillity.
    git-svn-id: https://plugins.svn.wordpress.org/seo-friendly-table-of-contents/trunk@590472 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,12 +3,12 @@ Contributors: tnyholm
 Tags: toc, Post, page, content, table of contents, seo, links
 Donate link: http://www.webfish.se/wp/donate
 Requires at least: 2.8.1
-Tested up to: 3.0.1
+Tested up to: 3.4.1
 Stable tag: trunk
 
 A simple seo friendly table of contents plugin that does not require editing in your themes source code.
 == Description ==
-This plugin puts a table of contents on your page. You just need to type `[toc="2,3" title="Table of contents"]` in the page/post editor. That's it!
+This plugin puts a table of contents on your page. You just need to type `[toc levels=3 title="Table of contents"]` in the page/post editor. That's it!
 You dont need to edit your themes source code. The plugin allows you to set a title of the toc box. You can also choose which heading tags (h1,h2,h3,h4 etc)
 you want to have in the table of contents. For more information see installation page or the 
 [Webfish](http://www.webfish.se/wp/plugins/seo-friendly-table-of-contents "Webfish - Wordpress plugin") homepage.
@@ -19,7 +19,7 @@ This is some brief installation notes. For more information see [Webfish](http:/
 
 1. Upload the plugin source to the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Put `[toc="2,3" title="Table of contents"]` on a page with at least one h2 heading.
+1. Put `[toc levels=2 title="Table of contents"]` on a page with at least one h2 or h3 heading.
 
 == Frequently Asked Questions ==
 
--- seo-friendly-table-of-contents.php
+++ seo-friendly-table-of-contents.php
@@ -2,8 +2,8 @@
 /*
  Plugin Name: Seo Friendly Table of Contents
  Plugin URI: http://www.webfish.se/wp/plugins/seo-friendly-table-of-contents
- Description: Adds a seo firendly table of contents anywhere you write [toc="2,3,4" title="Table of contents"].
- Version: 1.3.7
+ Description: Adds a seo firendly table of contents anywhere you write [toc levels=2 title="Table of contents"].
+ Version: 2.0.0
  Author: Tobias Nyholm
  Author URI: http://www.tnyholm.se
  Copyright: Tobias Nyholm 2010
@@ -26,107 +26,119 @@
  */
 
 
-add_filter('the_content', 'seotocFilter');
+add_filter('the_content', 'seotoc_contentFilter');
 
 //add css
-add_action('wp_enqueue_scripts','seotok_addStyle');
-function seotok_addStyle(){
+add_action('wp_enqueue_scripts','seotoc_addStyle');
+function seotoc_addStyle(){
 	wp_enqueue_style('toc_css',WP_PLUGIN_URL.'/seo-friendly-table-of-contents/style.css');
 }
 
+function seotoc_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+			'levels' => '2',
+			'title'=> false,
+	), $atts ) );
+		
+	$tags=array_slice(array(2,3,4,5,6,7),0,$levels);
+	$toc=getSeo_toc($tags,$title);
+
+	return $toc;
+}
+add_shortcode('toc','seotoc_shortcode');
+
+
 /**
- * Verifies if a table of contents should be here. If so, then we add one.
+ * Writes an id tag to every heading
  * @param String $content
  */
-function seotocFilter($content){
-
-	//reges to match if the "[toc=2]" exists
-	$tolc_regex = '|(?:<p>)?\[toc=(?:["'."'".'])?([1-7,]+)(?:["'."'".'])?(?: title=(?:["'."'".'])?(.*?)(?:["'."'".'])?)? ?\](?:<br \/>)?(?:<\/p>)?|s';
-	$tolc_match = preg_match_all($tolc_regex, $content, $tolc_matches);
-	//echo ("<pre>".print_r($tolc_matches,true));
-
-	//if [toc] exists
-	if ($tolc_match){
-		//split toc parameters into a array
-		$tags=split(",",$tolc_matches[1][0]);
-
-		if($tags[count($tags)-1]=="")//if the user has a tailoring coma
-		unset($tags[count($tags)-1]);
-		//sort
-		sort($tags);
-
-		//assert: $tags[0] is the first heading to appear.
-
-		//get the title
-		$toc_title=$tolc_matches[2][0];
-		$list="";//allcolate some space
-
-		//ad ids to the headings
-		seotoc_addIds($tags,$content,$list);
-
+function seotoc_contentFilter($content){
+	$tags="234567";
 	
-		//wrapp the list into some divs and add the title if the list was not empty
-		if($list!=""){
-			global $post;
-			$list="<div id='toc' class='post-".$post->ID."'><div id='toc_title'>$toc_title</div>$list</div>";
-		}
+	//some regex to recognize the h#-tags
+	$tag_regex = '/<h(['.$tags.'])(.*?)>(.*?)(<\/h['.$tags.']>)/';
+	
+	$tag_match = preg_match_all($tag_regex, $content, $tag_matches);
+	//die ("<pre>".print_r($tag_matches,true));
+	
+	$lastLevel=-1;//init leven
+	
+	//if a h#-tag found.
+	if ($tag_match){
+		foreach ($tag_matches[3] as $key => $title){
+			//original string
+			$orgStr=$tag_matches[0][$key];
+	
+			//strip any unwanted html
+			$titleNoTags=strip_tags($title);	
+			$titleSlug=seotoc_slugify($titleNoTags);
 			
-		//replace the [toc] with the $html
-		$content = preg_replace($tolc_regex, $list, $content,1);
+				
+			//this is the h#-tag
+			$tag="h".$tag_matches[1][$key];
+				
+			//save any attributes that the h#-tag allredy has. and then add the id.
+			$attributes=$tag_matches[2][$key]." id='$titleSlug'";				
+	
+
+			//replace the original heading with the one with an ID
+			$content = str_replace($orgStr, "<$tag$attributes>$title</$tag>", $content);
+		}
 	}
 
 	return $content;
 }
 
 /**
- * Add the some ids on h#-tags. It also build a html list with links to those tags.
- * @param array $tagsArray, sorted
- * @param String $content, 
- * @param String $list, when the function has been called, this is a html list with heading links (if any)
- * @return nothing $content and $list has the result
+ * kind of ugly slugify. But it works well with special chars
  */
-function seotoc_addIds(array $tagsArray,&$content,&$list){
+function seotoc_slugify($str){
+	$replace=array("/\&#[0-9]*?;/"=>"","/[^a-z A-Z0-9]/"=>"","/ /"=>"-");
+	$str=preg_replace(array_keys($replace),array_values($replace), $str);
+	return $str;
+}
+
+/**
+ * This will return a table of contents. 
+ * 
+ * @param array $tags like array(2,3,4)
+ * @param String $title
+ */
+function getSeo_toc(array $tagsArray,$toc_title=null){
+	global $post;
+	$content=$post->post_content;
+	
 	$tags="";
 	//make a string form the array
 	foreach($tagsArray as $tag)
 		if($tag<8)
-			$tags.="$tag";
-		
+		$tags.="$tag";
+	
+	if(!$toc_title)
+		$toc_title="";
+	else
+		$toc_title="<div id='toc_title' class='post-".$post->ID."'>$toc_title</div>";
+	
+	
 	//some regex to recognize the h#-tags
 	$tag_regex = '/<h(['.$tags.'])(.*?)>(.*?)(<\/h['.$tags.']>)/';
-
+	
 	$tag_match = preg_match_all($tag_regex, $content, $tag_matches);
 	//die ("<pre>".print_r($tag_matches,true));
 	
 	$lastLevel=-1;//init leven
-	$list="";//override $list
+	$list="";//create $list
 	
 	//if a h#-tag found.
 	if ($tag_match){
 		foreach ($tag_matches[3] as $key => $title){
-			/*
-			 * Prepare some variables
-			 */
-			//original string
-			$orgStr=$tag_matches[0][$key];
-				
 			//strip any unwanted html
-			$noTagsTitle=strip_tags($title);
+			$titleNoTags=strip_tags($title);	
+			$titleSlug=seotoc_slugify($titleNoTags);
 			
-			//kind of ugly slugify. But it works well with special chars
-			$replace=array("/\&#[0-9]*?;/"=>"","/[^a-z A-Z0-9]/"=>"","/ /"=>"-");
-			$noSpecial= preg_replace(array_keys($replace),array_values($replace), $noTagsTitle);
-			
-			//this is the h#-tag
-			$tag="h".$tag_matches[1][$key];
-			
-			//save any attributes that the h#-tag allredy has. and then add the id.
-			$attributes=$tag_matches[2][$key]." id='$noSpecial'";
-			
-			//Set a level. 
+			//Set a level.
 			$thisLevel=$tag_matches[1][$key];
 			
-
 			/*
 			 * prepare list
 			*/
@@ -135,68 +147,30 @@ function seotoc_addIds(array $tagsArray,&$content,&$list){
 				$lastLevel=$thisLevel;
 			}
 			elseif($thisLevel<$lastLevel){//end a sublist
-				$list.="</li>";//end the last li on prev listlevel. 
+				$list.="</li>";//end the last li on prev listlevel.
 				for($i=$lastLevel;$i>$thisLevel;$i--)
 					$list.="</ul></li>"."\n";
-				$lastLevel=$thisLevel;
+					$lastLevel=$thisLevel;
 			}
 			else{//end a list item
 				$list.='</li>'."\n";
 			}
-			
+				
 			//add a list item
-			$list.='<li><a href="#'.$noSpecial.'">'.$noTagsTitle.'</a>';//no closing li
+			$list.='<li><a href="#'.$titleSlug.'">'.$titleNoTags.'</a>';//no closing li
 				
-			/*
-			 * write id
-			 */
-			//replace the original heading with the one with an ID
-			$content = str_replace($orgStr, "<$tag$attributes>$title</$tag>", $content);
-				
-		}
+		}//foreach
+		
 		$list.='</li>'."\n";//close the last opened li
 		for($i=$lastLevel;$i>$tagsArray[0];$i--)//close all open tags
 			$list.="</ul></li>";
 		$list.= "</ul>\n";//close the last ul
-	}
-	//No need to return anyting. The $content and $list parameter is passed by reference.
-}
-
-
-
-/**
- * This will return a table of contents. The third param ($content) is send by reference. 
- * This function will add id attributes on the heading tag. You have to use the same content
- * variable to display your content as the one you use to this function. 
- * 
- * If you are using get_the_content() to get the content for this function, then you might
- * want to apply some filters before echoing out the content to the browser. Consider these lines:
- * 
- * $the_content = get_the_content();
- * $the_content = apply_filters('the_content', $the_content);
- * $the_content = str_replace(']]>', ']]&gt;', $the_content);
- * echo getSeo_toc(array(2,3,4),"Table of contents",$the_content);
- * echo $the_content;
- * 
- * @param array $tags like array(2,3,4)
- * @param String $title
- * @param String $content, the page content. The page content will be changed!
- */
-function getSeo_toc(array $tags,$title, &$content){
-	if($title==""||$title==false||$title==null){
-		$title="";
-	}
-	else{
-		global $post;
-		$title="<div id='toc_title' class='post-".$post->ID."'>$title</div>";
-	}
+		
+		$list="<div id='toc'>$toc_title$list</div>";
+		
+	}//if ($tag_match)
 	
-	$list="";
-	seotoc_addIds($tags,$content,$list);
-	if($list!="")
-		$list="<div id='toc'>$title$list</div>";
+	
 	
 	return $list;
 }
-
-?>
